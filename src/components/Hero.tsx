c5b72b2048b3082cd317fdb50212a32f84391119
Niko Pastore <nikopastore@gmail.com>
2025-12-21 15:03:40 -0700

    Reduce hero height to 85vh to show content below

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ import { siteConfig } from "@/lib/data";
 
 export default function Hero() {
   return (
-    <section className="min-h-screen pt-16 flex flex-col">
+    <section className="h-[85vh] pt-16 flex flex-col">
       {/* Physics Canvas Container - Full width edge to edge */}
       <div className="flex-grow w-full">
         <PhysicsCanvas />
